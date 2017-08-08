2be60916f38d964ef7fb48cd90b952739b93b270
Chester <chester@amourgis.com>
2017-08-08 15:38:59 -0400

    Add project files.

---
+++ LegalDB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LegalDB.MvcApplication" Language="C#" %>
---
+++ LegalDB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LegalDB.Startup))]
+namespace LegalDB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
